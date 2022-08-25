b289622aac29f2ba16169512c43f221875925b45
Artemiza Rocha <miza-pma@hotmail.com>
2022-08-25 14:39:07 -0300

    requisição http

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import './style.css';
+ 
 function App() {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(()=> {
+
+    function loadApi(){
+      let url = 'https://sheet.best/api/sheets/f1a2f00b-2754-4382-bb3c-b4d000697f8d';
+
+      fetch(url)
+      .then((r)=> r.json())
+      .then((json)=> {
+        console.log(json);
+        setNutri(json);
+      })
+    }
+  
+    loadApi();
+ }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <strong>Ábitos Saudáveis</strong>
       </header>
-    </div>
+      
+      {nutri.map((item)=>{
+        return(
+           <article key={item.id} className="post">
+           <strong className="titulo">{item.titulo}</strong>
+          
+          <img src={item.capa} alt={item.titulo} className="capa"/>
+          <p className="subtitulo">
+            {item.subtitulo}
+          </p>
+          <a className="botao">Acessar</a>
+
+        </article>
+
+      )
+    })}
+   </div>
   );
 }
 
